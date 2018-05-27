350591d05c49699c8fce04cebe51cb0c24872d3d
td72 <teledx72@gmail.com>
2018-05-28 07:05:20 +0900

    update templates

--- .config/nvim/template/template.cpp
+++ .config/nvim/template/template.cpp
@@ -1,6 +1,5 @@
 #include <iostream>
 
-using namespace std;
-
 <+CURSOR+>
+
 // vim:set fenc=utf-8 ff=unix expandtab sw=4 ts=4:
---
+++ .config/nvim/template/template.h
@@ -0,0 +1,6 @@
+#ifndef <+GUARD+>_H
+#define <+GUARD+>_H
+
+<+CURSOR+>
+
+#endif // <+GUARD+>_H
